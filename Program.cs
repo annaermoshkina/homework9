76fde390a55f56a304a58cc71a04c4c8e6bd480c
annaermoshkina <118661438+annaermoshkina@users.noreply.github.com>
2022-12-05 14:54:08 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+int recursion(int m,int n)
+{
+    if (m==0)
+    {
+        return n+1;
+    }
+    else if (n==0 && m>0)
+    {
+        return recursion(m-1,1);
+    }
+    else
+    {
+        return recursion(m-1,recursion(m,n-1));
+    }
+}
+Console.WriteLine("Введите первое число");
+int n=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите второе число");
+int m=Convert.ToInt32(Console.ReadLine());
+Console.Write(recursion(m,n));
